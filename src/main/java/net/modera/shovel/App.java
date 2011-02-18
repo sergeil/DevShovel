1aafe20599cbee61d39bdc47d4ed35b4242e30af
Aleksandr Rudakov <aleksandr@itpeople.ee>
2011-02-18 17:20:39 +0200

    Deps for zest

---
+++ src/main/java/net/modera/shovel/App.java
@@ -0,0 +1,41 @@
+package net.modera.shovel;
+
+import org.eclipse.zest.core.widgets.Graph;
+import org.eclipse.zest.core.widgets.GraphConnection;
+import org.eclipse.zest.core.widgets.GraphNode;
+import org.eclipse.zest.layouts.LayoutStyles;
+import org.eclipse.zest.layouts.algorithms.SpringLayoutAlgorithm;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+
+public class App {
+	public static void main(String[] args) {
+		
+		Display d = new Display();
+		Shell shell = new Shell(d);
+		shell.setText("GraphSnippet1");
+		shell.setLayout(new FillLayout());
+		shell.setSize(400, 400);
+
+		Graph g = new Graph(shell, SWT.NONE);
+
+		GraphNode n = new GraphNode(g, SWT.NONE, "Paper");
+		GraphNode n2 = new GraphNode(g, SWT.NONE, "Rock");
+		GraphNode n3 = new GraphNode(g, SWT.NONE, "Scissors");
+		new GraphConnection(g, SWT.NONE, n, n2);
+		new GraphConnection(g, SWT.NONE, n2, n3);
+		new GraphConnection(g, SWT.NONE, n3, n);
+		g.setLayoutAlgorithm(new SpringLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
+
+		shell.open();
+		while (!shell.isDisposed()) {
+			while (!d.readAndDispatch()) {
+				d.sleep();
+			}
+		}
+		
+	}
+}
